bea1bc33cbc0bcfbd3019c0f5b35037c842c9643
github username <github email address>
2018-06-18 17:13:39 -0400

    updated show action of flights controller to render json and added a div to user show view

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+$(function(){
   $("a.my_flights").on("click", function(e){
-    alert("have you ever seen a crowd go ape shit?")
+    $.get(this.href).success(function(response){
+      $("div.flights").html(response)
+    })
+
+    $.get(this.href).success(function(json){
+
+      var $ol = $("div.flights ol")
+        $ol.html(" ")
+        json.forEach(function(flight){
+        $ol.append(flight.destination_city)
+      })
+  })
     e.preventDefault();
   })
 })
