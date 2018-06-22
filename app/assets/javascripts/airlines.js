63d19985f0a2a7f881af7f6ae00e57a5762f8f5c
evanmc21 <emcandler21@gmail.com>
2018-06-22 13:49:11 -0400

    rendered json in the airlines create controller action

--- app/assets/javascripts/airlines.js
+++ app/assets/javascripts/airlines.js
@@ -15,3 +15,36 @@
 //     e.preventDefault();
 //   })
 // })
+$(function(){
+  $("a.my_airlines").on("click", function(e){
+
+    e.preventDefault();
+  })
+})
+
+$(function(){
+  $("#new_airline").on("submit", function(e){
+    // alert("wtf is up kyle?")
+    url = this.action
+    console.log(url)
+
+    data = {
+      'authenticity_token': $("input[name='authenticity_token']").val(),
+      'airline': {
+        'name': $("#airline_name").val(),
+        'rewards_number': $("#airline_rewards_number").val()
+      }
+    }
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: data,
+      success: function(response){
+        var $ol = $("div.airlines ol")
+        $ol.append(response);
+      }
+    })
+    e.preventDefault();
+  })
+})
