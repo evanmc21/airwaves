cef74a4bb56bcbb975d49032358c7ecaba46759e
github username <github email address>
2018-09-04 10:24:53 -0400

    got rid of airlines javascript and added filter airlline feature. also updated format of flights on user show page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,7 +9,7 @@ $(function(){
         flights.forEach(function(flight){
           let newFlights = new Flights(flight)
           let flightsHTML = newFlights.formatFlight()
-          $("div.flights").append(flightsHTML)
+          $("div.flights").prepend(flightsHTML)
           // debugger
         })
       })
@@ -27,12 +27,14 @@ function Flights(flight) {
 Flights.prototype.formatFlight = function(){
   let userID = parseInt(window.location.href.split("/")[4])
   let flightsHTML = `
+  <div class="flights-float" style="float:left; margin: 5px">
     <ol>
       <h3>Departure ${this.departure}</h3>
       <p>origin: ${this.originCity}</p>
       <p>destination: ${this.destinationCity}</p>
       <a href="/users/${userID}/flights/${this.id}">flight details</a>
     </ol>
+  </div>
   `
   return flightsHTML
 }
